764ea0f47024c5100cf2cff70ea6b97066ad1105
David <davidongaroh@gmail.com>
2022-09-28 16:34:00 +0300

    solved

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
-
-  def index
-    items = Item.all
-    render json: items, include: :user
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+def index
+  if params[:user_id]
+    user = User.find(params[:user_id])
+    items = user.items
+  else 
+    items=Item.all
   end
+    render json: items, include: :user
+  
+end
 
+  def show
+      item = Item.find(params[:id])
+      render json: item, include: :user
+    
+      end
+
+  def create
+    user = User.find(params[:user_id])
+    item = Item.create(item_params)
+    item.user = user
+    render json: item, include: :user, status: :created
+  end
+  
+private
+def item_params
+  params.permit(:name, :description, :price, :user_id)
+end
+
+def render_not_found_response
+  render json: { error: "Item not found" }, status: :not_found
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :items, only: [:index, :show]
+  resources :users, only: [:index,:show] do
+    resources :items, only: [:index, :show, :create]
+  end
 end
